118e9823d0250319c0f5cbe6e4901b3e951c032f
Spencer Barton <sbarton272@gmail.com>
2018-04-11 23:06:30 -0700

    Terraform

---
+++ terraform/tutorial.tf
@@ -0,0 +1,19 @@
+# AWS setup
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = "/Users/spencer/.aws/credentials"
+  profile                 = "terraform"
+}
+
+resource "aws_instance" "terraform_tutorial_new" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "terraform_tutorial"
+  }
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.terraform_tutorial_new.id}"
+}
